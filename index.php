92408512156cca1c87b3ebbe87acaf0b5c3608c1
intheblueside <ishwarimani2501@gmail.com>
2024-04-10 23:54:24 +0800

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home Page</title>
+
+    </head>
+    <body>
+        <!--header-->
+        <?php
+            include 'base.php';
+        ?>
+
+        
+        
+    </body>
+</html>
