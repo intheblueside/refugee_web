5b8b55e3f90c0ad728435057ccfa8a22673c2f96
intheblueside <ishwarimani2501@gmail.com>
2024-04-11 00:38:52 +0800

    updated footer in separate page

--- index.php
+++ index.php
@@ -1,7 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home Page</title>
+
+        <style>
+            * {box-sixing: border-box;}
+            body {font-family: Helvetica, sans-serif;}
+            .mySlider {display:none;}
+            img {vertical-align: middle;}
+
+            /* Slideshow container */
+            .slideshow-container {
+            max-width: 1000px;
+            position: relative;
+            margin: auto;
+            }
+
+            /* Caption text */
+            .text {
+            color: #f2f2f2;
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+            }
+
+            /* Number text (1/3 etc) */
+            .numbertext {
+            color: #f2f2f2;
+            font-size: 12px;
+            padding: 8px 12px;
+            position: absolute;
+            top: 0;
+            }
+
+            /* The dots/bullets/indicators */
+            .dot {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+            }
+
+            .active {
+            background-color: #717171;
+            }
+
+            /* Fading animation */
+            .fade {
+            animation-name: fade;
+            animation-duration: 1.5s;
+            }
+
+            @keyframes fade {
+            from {opacity: .4} 
+            to {opacity: 1}
+            }
+        </style>
 
     </head>
     <body>
@@ -9,8 +71,63 @@
         <?php
             include 'base.php';
         ?>
-
         
+        <main>
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <div class="numbertext">1 / 3</div>
+                        <img src="r1.jpg" alt="test1" style="width:100%">
+                        
+                </div>
+                <div class="mySlides fade">
+                    <div class="numbertext">1 / 3</div>
+                        <img src="r2.jpg" alt="test2" style="width:100%">
+                        
+                </div>
+                <div class="mySlides fade">
+                    <div class="numbertext">1 / 3</div>
+                        <img src="r3.jpg" alt="test3" style="width:100%">
+                        
+                </div>
+            </div>
+
+            <br>
+
+            <div style="text-align:center">
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+            </div>
+
+        </main>
+
+        <script>
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+        }
+        </script>
+
+        <footer>
+            <?php 
+                include 'footer.php';
+            ?>
+        </footer>
         
     </body>
 </html>
