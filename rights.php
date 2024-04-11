96d0ae48d1081ea6471f40ca00400be9228e48db
intheblueside <ishwarimani2501@gmail.com>
2024-04-12 00:09:41 +0800

    new page our rights added

---
+++ rights.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Our Rights</title>
+    </head>
+    <body>
+        <!--include header-->
+        <nav>
+            <?php
+                include 'base.php';
+            ?>
+        </nav>
+
+        <!--main content-->
+        <main>
+
+
+        <footer>
+            <!--include footer php-->
+            <?php 
+                include 'footer.php';
+            ?>
+        </footer>
+
+    </body>
+</html>
