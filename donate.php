606c7aea28a95ca79965131fbbd85e6eb3216981
intheblueside <ishwarimani2501@gmail.com>
2024-04-14 00:13:27 +0800

    cards complete build

--- donate.php
+++ donate.php
@@ -28,21 +28,85 @@
 
     <main>
         <h2 class="text-center display-4">Help Refugees</h2>
-            <!--section volunteer-->
-            <section id="primary" class="content-area">
-                <div class="container my-5">
-                    <div class="row g-2">
-                        <div class="col-lg-12">
-                            <h5>You can support refugee rights by donating to RefugeeRights, giving refugees the help for safety
+            <!--section main heading-->
+        <section id="primary" class="content-area">
+            <div class="container my-5">
+                <div class="row g-2">
+                    <div class="col-lg-12">
+                        <h5>You can support refugee rights by donating to RefugeeRights, giving refugees the help for safety
                                 and opportunity and also bringing them closer to their freedom
-                            </h5>
-                            <div class="image-fluid">
-                                <img src="images/happy.jpg" alt="happy refugee children" style="width: 100%;">
+                        </h5><hr>
+                        <div class="image-fluid">
+                            <img src="images/happy.jpg" alt="happy refugee children" style="width: 100%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--section donation-->
+        <section>
+            <div class="container my-5">
+                <div class="row g-2">
+                    <!--volunteer card-->
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <img src="images/happy.jpg" class="card-img-top" alt="volunteer">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h5 class="card-title">Volunteer with us</h5>
+                                    <p class="card-text">Volunteers fill vital roles in our national organizations. They offer support to forcibly displaced clients on stages both large and small 
+                                        providing advice, encouragement and solidarity in the face of country-wide discrimination. </p>
+                                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#volunteerModal">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--volunteer card-->
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <img src="images/happy.jpg" class="card-img-top" alt="volunteer">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h5 class="card-title">Donate</h5>
+                                    <p class="card-text">yaydayaydaydya</p>
+                                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#volunteerModal">Learn More</a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <!--volunteer card-->
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <img src="images/happy.jpg" class="card-img-top" alt="volunteer">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h5 class="card-title">Sign the petition</h5>
+                                    <p class="card-text">yaydayaydaydya</p>
+                                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#volunteerModal">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--volunteer card-->
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <img src="images/happy.jpg" class="card-img-top" alt="volunteer">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h5 class="card-title">Partner with us</h5>
+                                    <p class="card-text">yaydayaydaydya</p>
+                                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#volunteerModal">Learn More</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
+
+
     </main>
 
 
