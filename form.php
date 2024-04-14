d8fbe0b8d0cb6611e8ff57ff6436d79e1118747e
intheblueside <ishwarimani2501@gmail.com>
2024-04-15 00:07:16 +0800

    amount value updated

--- form.php
+++ form.php
@@ -13,8 +13,20 @@
         <title>Donate For Refugees</title>
 
         <style>
-            .error {color: #FF0000;}
+            .error {
+            color: #FF0000;
+            margin-top: 0.25rem;
+            font-size: 0.875rem;
+            }
 
+            .form-group.row {
+                margin-bottom: 1rem; /* Adjust as needed */
+            }
+
+            .form-group.row .col-sm-3 {
+                text-align: right;
+                padding-top: calc(0.375rem + 1px); /* To vertically center the label */
+            }
             .row {
                 background-color: lavenderblush;
             }
@@ -25,15 +37,50 @@
     <body>
 
     <?php
-    $firstErr = $lastErr = $emailErr = $contactErr = $commentErr = $suffixErr = $howErr = "";
-    $first = $last = $email = $contact = $comment = $suffix = $how = "";
+    $firstErr = $lastErr = $emailErr = $contactErr = $suffixErr = $howErr = $checkErr = "";
+    $first = $last = $email = $contact = $suffix = $how = $checkbox = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (empty($_POST["firstname"])) {
-            $firstErr = "Name is required!!";
+            $firstErr = "First Name is required!!";
         } else {
             $first = test_input($_POST["firstname"]);
-            
+        }
+
+        if (empty($_POST["lastname"])) {
+            $lastErr = "Last Name is required!!";
+        } else {
+            $last = test_input($_POST["lastname"]);
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required!!";
+        } else {
+            $email = test_input($_POST["email"]);
+        }
+
+        if (empty($_POST["contact"])) {
+            $contactErr = "Contact Number is required!!";
+        } else {
+            $contact = test_input($_POST["contact"]);
+        }
+
+        if (empty($_POST["suffix"])) {
+            $suffixErr = "Suffix is required!!";
+        } else {
+            $suffix = test_input($_POST["suffix"]);
+        }
+
+        if (empty($_POST["how"])) {
+            $howErr = "This is required!!";
+        } else {
+            $how = test_input($_POST["how"]);
+        }
+
+        if (empty($_POST["check-donate"])) {
+            $checkErr = "This is required!!";
+        } else {
+            $checkbox = test_input($_POST["check-donate"]);
         }
     }
 
@@ -74,15 +121,15 @@
 
                         <h4 class="section-header-container">Select your one-time amount</h4>
                         <div class="mb-3 form-check">
-                            <button type="button">$250</button>
-                            <button type="button">$150</button>
-                            <button type="button">$100</button>
-                            <button type="button">$50</button>
-                            <button type="button">$25</button>
+                            <button type="button" onclick="updateAmount(250)">$250</button>
+                            <button type="button" onclick="updateAmount(150)">$150</button>
+                            <button type="button" onclick="updateAmount(100)">$100</button>
+                            <button type="button" onclick="updateAmount(50)">$50</button>
+                            <button type="button" onclick="updateAmount(25)">$25</button>
                             <!--user input amount-->
                             <div class="input-group mb-3">
                                 <span class="input-group-text">$</span>
-                                <input type="text" id="amount"/>
+                                <input type="text" class="form-control" id="amountInput" aria-describedby="user-entered-amount" oninput="updateAmount(this.value)">
                                 <span class="input-group-text">.00</span>
                             </div>
                             
@@ -103,14 +150,15 @@
                             <div class="col-md-6">
 
                                 <div class="form-group col-md-6">
-                                    <label for="firstname" class="form-label">First Name
+                                    <label for="firstname" class="form-label">First Name</label>
                                     <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="user-first-name">
-                                    </label><span class="error">* <?php echo $firstErr; ?> </span> 
+                                    <span class="error">* <?php echo $firstErr; ?> </span> 
                                 </div>
                                 
                                 <div class="col-md-6">
                                     <label for="lastname" class="form-label">Last Name</label>
                                     <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="user-last-name">
+                                    <span class="error">* <?php echo $lastErr; ?> </span>
                                 </div>
         
                                 <div class="col-md-6">
@@ -122,27 +170,31 @@
                                         <option value="3">Ms</option>
                                         <option value="4">Dr</option>
                                     </select>
+                                    <span class="error">* <?php echo $suffixErr; ?> </span>
                                 </div>
                                 
                                 <div class="col-md-6">
                                     <label for="email" class="form-label">Email address</label>
                                     <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
                                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                    <span class="error">* <?php echo $emailErr; ?> </span>
                                 </div>
         
                                 <div class="col-md-6">
                                     <label for="contactnum" class="form-label">Contact Number</label>
                                     <input type="number" class="form-control" id="contactnum" name="contact" aria-describedby="user-contact">
+                                    <span class="error">* <?php echo $contactErr; ?> </span>
                                 </div>
         
                                 <div class="col-md-6">
                                     <label for="comment" class="form-label">Comment</label>
                                     <textarea name="comment" rows="5" cols="40" class="form-control" id="comment" name="comment" aria-describedby="user-comment"></textarea>
+                                    
                                 </div>
         
                                 <div class="col-md-6">
                                     <label for="information" class="form-label">How did you hear about us?</label>
-                                    <select class="form-select mb-3" id="information" name="how" aria-label="Default select example">
+                                    <select class="form-select mb-6" id="information" name="how" aria-label="Default select example">
                                         <option selected>Select One</option>
                                         <option value="1">Search Engine</option>
                                         <option value="2">Social Media</option>
@@ -151,16 +203,19 @@
                                         <option value="5">Word of mouth</option>
                                         <option value="6">Other</option>
                                     </select>
+                                    <span class="error">* <?php echo $howErr; ?> </span>
                                 </div>
         
                                 <hr>
                                 <div class="col-md-6">
                                     <div id="amountDetails" class="form-text">One-time donation</div>
-                                    <label for="amount" class="form-label">$0.00 USD</label>
+                                    <label for="amount" class="form-label" id="amountLabel">$0.00 USD</label>
+                                    
                                 </div>
                                 <div class="mb-3 form-check">
                                     <input type="checkbox" class="form-check-input" id="check-donate">
-                                    <label class="form-check-label" for="exampleCheck1">I confirm that all information in here are accurate</label>
+                                    <label class="form-check-label" for="confirmCheck" name="check-donate">I confirm that all information in here are accurate</label>
+                                    <span class="error">* <?php echo $checkErr; ?> </span>
                                 </div>
                                 <button type="submit" name="submit" class="btn btn-primary">Pay Now</button>
         
@@ -174,6 +229,13 @@
                 </div>
             </div>
         </div>
+
+        <script>
+
+            function updateAmount(amount) {
+                document.getElementById("amountLabel").textContent = "$" + parseFloat(amount).toFixed(2) + " USD";
+            }
+        </script>
 
     </body>
 </html>
